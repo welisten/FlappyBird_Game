dae7dbb7fb9e0f7df232c1501f418b3a035c354a
Wesley Welisten <106595542+welisten@users.noreply.github.com>
2023-12-24 03:49:47 -0300

    feat: FlappyBird_Game - Positive test to responsivity

--- js/orientation.js
+++ js/orientation.js
@@ -1,17 +1,28 @@
+
 const flappyContainer = document.querySelector('[wm-flappy]')
 const titleContainer = document.querySelector('.title')
 const modalContainer = document.querySelector('#fade')
+
+const screenWidth  = document.documentElement.clientWidth 
+const screenHeight = document.documentElement.clientHeight
 
 
 console.log(flappyContainer, titleContainer, modalContainer)
 function adjustScreen(){
     if(window.innerHeight > window.innerWidth){
         flappyContainer.style.transform = `rotate(90deg)`
-        titleContainer.style.transform = `rotate(90deg)`
-        modalContainer.style.transform = `rotate(90deg)`
+        // titleContainer.style.display = `none`
+        // modalContainer.style.transform = `rotate(90deg)`
+
+        flappyContainer.style.width  = `${screenHeight}px` 
+        flappyContainer.style.height = `${screenWidth}px`
+        modalContainer.style.width  = `${screenHeight}px` 
+        modalContainer.style.height = `${screenWidth}px`
+
+        console.log(screenHeight, screenWidth)
     }else{
         flappyContainer.style.transform = `rotate(0deg)`
-        titleContainer.style.transform = `rotate(0deg)`
+        // titleContainer.style.diplay = `contents`
         modalContainer.style.transform = `rotate(0deg)`
     }
 }

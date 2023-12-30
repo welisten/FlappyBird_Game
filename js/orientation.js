7cb74331909e6043224a55a3b6d3a62a16b03672
Wesley Welisten <welistenw@gmail.com>
2023-12-30 11:05:20 -0300

    feat: FlappyBird_Game - fixing CSS

--- js/orientation.js
+++ js/orientation.js
@@ -1,20 +1,82 @@
+
 const flappyContainer = document.querySelector('[wm-flappy]')
 const titleContainer = document.querySelector('.title')
 const modalContainer = document.querySelector('#fade')
+const conteudo = document.querySelector('.conteudo')
 
-console.log(flappyContainer, titleContainer, modalContainer)
-function adjustScreen() {
-    if( window.innerHeight > window.innerWidth){
-        flappyContainer.style.transform = 'rotate(90deg)'
-        modalContainer.style.transform = 'rotate(90deg)'
-    }else(
-        flappyContainer.style.transform = 'rotate( 0deg )',
-        modalContainer.style.transform = 'rotate(0deg)'
-    )
+const screenOuterWidth  = window.outerWidth
+const screenOuterHeight = window.outerHeight
+const screenWidth  = window.innerWidth
+const screenHeight = window.innerHeight
+
+
+console.log(screenHeight, screenHeight, outerHeight, outerWidth)
+function adjustScreen(){
+    if(window.innerHeight > window.innerWidth){
+        const styleTag = document.createElement('link')
+        styleTag.setAttribute('href', './css/orientation.css')
+        styleTag.setAttribute('rel', 'stylesheet')
+        document.head.appendChild(styleTag)
+        
+        const modalPlay = document.querySelector('#modal-play')
+        
+        const newModal = document.createElement('div')
+        newModal.classList.add('modal')
+        newModal.id = 'modal-warning'
+
+        const modalHeader = document.createElement('div')
+        modalHeader.classList.add('modal-header')
+        modalHeader.innerHTML = `
+            <h1>
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                ATENÇÃO
+                <i class="fa-solid fa-triangle-exclamation"></i>
+            </h1>
+        `
+        const modalBody = document.createElement('div')
+        modalBody.classList.add('modal-body')
+        modalBody.innerHTML = `
+            <p>
+                Utilise seu dispositivo na orientação horizontal para ter uma melhor experiência de jogo
+            </p>
+        `
+
+        const modalWarningSpan = document.createElement('span')
+        modalWarningSpan.classList.add('warning-span')
+        modalWarningSpan.innerHTML = `<i class="fa-solid fa-mobile-screen-button"></i>`
+
+        modalPlay.classList.toggle('hiden')
+        newModal.appendChild(modalHeader)
+        newModal.appendChild(modalBody)
+        modalBody.appendChild(modalWarningSpan)
+        modalContainer.appendChild(newModal)
+
+        flappyContainer.style.transform = `rotate(90deg)`
+        flappyContainer.style.width  = `${screenHeight - (screenHeight * 0.1)}px` 
+        flappyContainer.style.height = `${screenWidth - (screenWidth * 0.2)}px`
+
+        modalContainer.style.width  = `${screenHeight}px` 
+        modalContainer.style.height = `${screenWidth}px`
+
+    // } else if(window.innerWidth >= 768 && window.orientation != 0){
+    //     const styleTag = document.createElement('link')
+    //     styleTag.setAttribute('href', './css/orientation.css')
+    //     styleTag.setAttribute('rel', 'stylesheet') 
+    //     document.head.appendChild(styleTag)
+    } else {
+        flappyContainer.style.transform = `rotate(0deg)`
+        // titleContainer.style.diplay = `contents`
+        modalContainer.style.transform = `rotate(0deg)`
+    }
 }
-function lockPortraitOrientation() {
-    window.addEventListener('resize', adjustScreen)
+
+window.addEventListener('orientationchange', (e) => {
+    location.reload()
+})
+
+function lockPortraitOrientation(){
     adjustScreen()
+    window.addEventListener('resize', adjustScreen)
 }
 
 lockPortraitOrientation()

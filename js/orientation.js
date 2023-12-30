5f2497c182c0f30662555d55a3e4debf74b41980
Wesley Welisten <welistenw@gmail.com>
2023-12-30 10:50:46 -0300

    test

--- js/orientation.js
+++ js/orientation.js
@@ -1,21 +1,20 @@
-const body = document.querySelector('body')
+const flappyContainer = document.querySelector('[wm-flappy]')
+const titleContainer = document.querySelector('.title')
+const modalContainer = document.querySelector('#fade')
 
+console.log(flappyContainer, titleContainer, modalContainer)
+function adjustScreen() {
+    if( window.innerHeight > window.innerWidth){
+        flappyContainer.style.transform = 'rotate(90deg)'
+        modalContainer.style.transform = 'rotate(90deg)'
+    }else(
+        flappyContainer.style.transform = 'rotate( 0deg )',
+        modalContainer.style.transform = 'rotate(0deg)'
+    )
+}
 function lockPortraitOrientation() {
-    function adjustScreen() {
-        if( window.innerWidth < window.innerHeight){
-            body.style.transform = 'rotate(90deg)'
-            body.style.transform = 'translateY(1)'
-
-        }else(
-            body.style.transform  = 'rotate(0deg)'
-        )
-    }
-
-    window.addEventListener('resize', adjustScreen())
-
+    window.addEventListener('resize', adjustScreen)
     adjustScreen()
 }
 
 lockPortraitOrientation()
-
-console.log(window.innerWidth)
